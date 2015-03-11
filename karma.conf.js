2c9a0a007818b18e7afd150abc73172920634d0b
Ignacio Rivas <rivasign@gmail.com>
2015-03-11 14:46:27 +0000

    Add simple integration tests

---
+++ karma.conf.js
@@ -0,0 +1,65 @@
+var webpack = require('webpack');
+
+var configSettings = {
+  'normal': {},
+  'uglified': {
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin()
+    ]
+  }
+};
+
+module.exports = function(config) {
+  config.set({
+
+  basePath: '',
+
+  frameworks: ['mocha'],
+
+  files: [
+    'test/index.js'
+  ],
+
+  preprocessors: {
+    'test/*': ['webpack']
+  },
+
+  webpack: {
+    module: {
+      loaders : [
+          { test: /\.js?$/, exclude: /node_modules/, loader: 'babel-loader' }
+      ]
+    }
+  },
+
+  webpackMiddleware: {
+    stats: {
+      colors: true
+    },
+    quiet: true
+  },
+
+  reporters: ['progress'],
+
+  port: 9876,
+
+  colors: true,
+
+  logLevel: config.LOG_DISABLE,
+
+  autoWatch: true,
+
+  browsers: ['Chrome'],
+
+  captureTimeout: 60000,
+
+  singleRun: false,
+
+  plugins: [
+    require('karma-webpack'),
+    require('karma-mocha'),
+    require('karma-spec-reporter'),
+    require('karma-chrome-launcher')
+  ]
+  });
+};
---
+++ test/index.js
@@ -0,0 +1,38 @@
+import $ from 'jquery';
+import Backbone from 'backbone';
+import chai from 'chai';
+
+let expect = chai.expect;
+
+import Router from '../app/scripts/router';
+
+
+describe('a-test', function() {
+  it('should run a test', function() {
+    expect(1).to.equal(1);
+  });
+});
+
+
+describe('Integration tests', function() {
+  const router = new Router();
+  Backbone.history.start();
+
+  let $app = $('#js-app');
+
+
+  describe('Dashboard page sample testing', function() {
+    it('should render hello ignacio on first render', function() {
+      expect($app.text()).to.equal('Hello ignacio !');
+    });
+  });
+
+  describe('About page sample testing', function() {
+    it('should render YO im the about page', function() {
+      router.navigate('about', {trigger: true, replace: true});
+
+      expect($app.text()).to.equal('YO im the about page');
+    });
+  });
+
+});
